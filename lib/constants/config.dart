d320829e07d9bb2980bed0b28ace11574fa3741a
Usama-Ansari-Flutter-Git <jbondansari.4@gmail.com>
2026-05-02 09:50:03 +0500

    setup changes

--- lib/constants/config.dart
+++ lib/constants/config.dart
@@ -5,4 +5,9 @@ const brandColor = Color(0xff3ecf8e);
 const secondaryColor = Color(0xFF1B5D3F);
 const greyColor = Color(0xffd3d3d3);
 const redColor = Color(0xff800200);
+
+
+//Supabase Configration
+const supabaseUrl = "https://omgxnkrdwircbodlipka.supabase.co/rest/v1/";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9tZ3hua3Jkd2lyY2JvZGxpcGthIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1NDI3NjIsImV4cCI6MjA5MTExODc2Mn0.OTD8hlmN_rbzGhplhPqnKAWKsp9Vznlwth36M6e94_I";
  
